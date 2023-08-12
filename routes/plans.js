ce688232ef5fb7c57e79c565bfde24ee8d5c3842
that-one-tom <19203795+that-one-tom@users.noreply.github.com>
2023-08-12 17:58:16 +0200

    remove unused imports

--- routes/plans.js
+++ routes/plans.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const database = require('../database');
-const url = require('url');
 const { DateTime } = require("luxon");
-const { type } = require('os');
 
 module.exports = app => {
   /*
